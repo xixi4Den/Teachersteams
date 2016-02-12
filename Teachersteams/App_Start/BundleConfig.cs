d80e44cd8f17ee0492040ec02580965d358bb601
xixi4Den <xixi4.den@gmail.com>
2016-02-12 23:02:14 +0300

    remove jquery, add angular

--- Teachersteams/App_Start/BundleConfig.cs
+++ Teachersteams/App_Start/BundleConfig.cs
@@ -1,27 +1,23 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace Teachersteams
 {
     public class BundleConfig
     {
-        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            bundles.Add(new ScriptBundle("~/bundles/mainlibs")
+                .Include("~/Scripts/libs/modernizr-*")
+                .Include("~/Scripts/libs/angular/angular.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/bundles/libs")
+                .Include("~/Scripts/libs/ui-bootstrap-tpls-*")
+                .Include("~/Scripts/libs/angular/angular-animate.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap/bootstrap.css",
                       "~/Content/site.css"));
         }
     }
