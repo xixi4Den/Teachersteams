f9e9b61d802e1b0037e98d2eb076fa73d90b0bb1
xixi4Den <xixi4.den@gmail.com>
2016-02-23 23:20:07 +0300

    fix an functionality related to the active state of navbar items

--- Teachersteams.Presentation/Scripts/controllers/navigationController.js
+++ Teachersteams.Presentation/Scripts/controllers/navigationController.js
@@ -4,19 +4,4 @@ app.controller('ttNavigationController', function ($scope, $location) {
     $scope.$on('$routeChangeSuccess', function () {
         $scope.isCollapsed = true;
     });
-    $scope.getClass = function (path) {
-        if (path === '/') {
-            if ($location.path() === '/') {
-                return "active";
-            } else {
-                return "";
-            }
-        }
-
-        if ($location.path().substr(0, path.length) === path) {
-            return "active";
-        } else {
-            return "";
-        }
-    }
 });
