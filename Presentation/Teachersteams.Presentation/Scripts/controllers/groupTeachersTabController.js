176c09dd7a5b0843cf5b5e8daa2ab994313a4daa
xixi4Den <xixi4.den@gmail.com>
2016-04-17 00:15:29 +0300

    extract common logic for grid settings to gridHelper.js

--- Presentation/Teachersteams.Presentation/Scripts/controllers/groupTeachersTabController.js
+++ Presentation/Teachersteams.Presentation/Scripts/controllers/groupTeachersTabController.js
@@ -5,41 +5,15 @@ app.controller('ttGroupTeachersTabController', [
     'uiGridConstants',
     '$ttTeacherService',
     '$stateParams',
-    'SortingDirection',
-    function ($scope, ngDialog, uiGridConstants, $ttTeacherService, $stateParams, SortingDirection) {
-        var paginationOptions = {
-            PageNumber: 1,
-            PageSize: 10,
-            SortingColumn: null,
-            SortingDirection: null
-        };
-
-        var refreshHeight = function () {
-            var newHeight = Math.floor($scope.gridOptions.data.length * 50 + 30);
-            angular.element(document.getElementsByClassName('grid')[0]).css('height', (newHeight + 30) + 'px');
-            angular.element(document.getElementsByClassName('ui-grid-viewport')[0]).css('height', newHeight + 'px');
-        };
-
-        var getPage = function () {
-            $ttTeacherService.count($stateParams.groupId)
-                .then(function (response) {
-                    $scope.gridOptions.totalItems = response.data;
-                });
-
-            $ttTeacherService.get($stateParams.groupId, paginationOptions)
-                .then(function (response) {
-                    $scope.gridOptions.data = response;
-                    refreshHeight();
-                });
-        };
-
+    '$gridHelper',
+    function ($scope, ngDialog, uiGridConstants, $ttTeacherService, $stateParams, $gridHelper) {
         $scope.invite = function (e) {
             var dialog = ngDialog.open({ template: '/Teacher/Group/InviteTeacherDialog', controller: 'ttInviteTeacherDialogController' });
             dialog.closePromise.then(function (newGroup) {
                 var result = newGroup.value;
                 if ((typeof result !== "undefined") && result.hasOwnProperty('Id') && (typeof result.Id !== "undefined")) {
                     $scope.$broadcast('create_group', result);
-                    getPage();
+                    $gridHelper.getPage();
                 }
             });
         }
@@ -64,27 +38,11 @@ app.controller('ttGroupTeachersTabController', [
             ],
             onRegisterApi: function (gridApi) {
                 $scope.gridApi = gridApi;
-                $scope.gridApi.core.on.sortChanged($scope, function (grid, sortColumns) {
-                    if (sortColumns.length === 0) {
-                        paginationOptions.SortingColumn = null;
-                        paginationOptions.SortingDirection = null;
-                    } else {
-                        if (sortColumns.length > 1) {
-                            sortColumns[0].unsort();
-                            return; // because unsort() triggers sortChanged again
-                        }
-                        paginationOptions.SortingColumn = sortColumns[0].field;
-                        paginationOptions.SortingDirection = SortingDirection.getByCode(sortColumns[0].sort.direction);
-                    }
-                    getPage();
-                });
-                $scope.gridApi.pagination.on.paginationChanged($scope, function (newPage, pageSize) {
-                    paginationOptions.PageNumber = newPage;
-                    paginationOptions.PageSize = pageSize;
-                    getPage();
-                });
+                $scope.gridApi.core.on.sortChanged($scope, $gridHelper.sortChanged);
+                $scope.gridApi.pagination.on.paginationChanged($scope, $gridHelper.paginationChanged);
             }
         };
 
-        getPage();
+        $gridHelper.initialize($ttTeacherService.count, $ttTeacherService.get, [$stateParams.groupId], $scope.gridOptions);
+        $gridHelper.getPage();
     }]);
---
+++ Presentation/Teachersteams.Presentation/Scripts/directives/noItemsMessageDirective.js
@@ -0,0 +1,16 @@
+﻿angular.module('ttDirectives')
+    .directive('noItemsMessage', function ($compile, $timeout) {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attrs) {
+                var msg = (attrs.showEmptyMsg) ? attrs.showEmptyMsg : 'No items';
+                var template = "<p class='no-items' ng-hide='gridOptions.data.length'>" + msg + "</p>";
+                var tmpl = angular.element(template);
+                $compile(tmpl)(scope);
+                $timeout(function () {
+                    var s = angular.element(element[0].querySelector('.ui-grid-viewport'));
+                    s.append(tmpl);
+                }, 0);
+            }
+        };
+    })
