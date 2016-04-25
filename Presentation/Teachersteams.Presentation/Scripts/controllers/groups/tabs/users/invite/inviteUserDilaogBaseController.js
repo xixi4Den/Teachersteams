668c6f6447ec79dd8c87a26ba1725979e5777315
xixi4Den <xixi4.den@gmail.com>
2016-04-26 00:48:51 +0300

    add ability to chose a search type for at user invitation dialog and add "Only friends" search type

--- Presentation/Teachersteams.Presentation/Scripts/controllers/groups/tabs/users/invite/inviteUserDilaogBaseController.js
+++ Presentation/Teachersteams.Presentation/Scripts/controllers/groups/tabs/users/invite/inviteUserDilaogBaseController.js
@@ -5,8 +5,10 @@ app.controller('ttInviteUserDialogBaseController', [
     'limitToFilter',
     'ngToast',
     '$stateParams',
-    function ($scope, $vk, limitToFilter, ngToast, $stateParams) {
+    'UserSearchType',
+    function ($scope, $vk, limitToFilter, ngToast, $stateParams, UserSearchType) {
         $scope.result = null;
+        $scope.searchType = UserSearchType.Friends;
 
         function formInvitation() {
             var result = {};
@@ -15,7 +17,21 @@ app.controller('ttInviteUserDialogBaseController', [
             return result;
         }
 
-        $scope.getUsers = function (queryString) {
+        function findFriends(queryString) {
+            return $vk.simpleCall('friends.get', {
+                fields: "uid, first_name, last_name, photo_medium"
+            }).then(function (r) {
+                var result = _.filter(r.response, function (item) {
+                    var format1 = item.last_name + " " + item.first_name;
+                    var format2 = item.first_name + " " + item.last_name;
+                    return format1.indexOf(queryString) >= 0
+                        || format2.indexOf(queryString) >= 0;
+                });
+                return limitToFilter(result, 10);
+            });
+        }
+
+        function findUsers(queryString) {
             return $vk.simpleCall('users.search', {
                 q: queryString,
                 fields: "uid, first_name, last_name, photo_medium"
@@ -23,6 +39,16 @@ app.controller('ttInviteUserDialogBaseController', [
                 var result = r.response.slice(1);
                 return limitToFilter(result, 10);
             });
+        }
+
+        $scope.getUsers = function (queryString) {
+            if ($scope.searchType === UserSearchType.Friends) {
+                return findFriends(queryString);
+            }
+            if ($scope.searchType === UserSearchType.AllUsers) {
+                return findUsers(queryString);
+            }
+            throw new Error("unsupported search type");
         }
 
         $scope.cancel = function (e) {
---
+++ Presentation/Teachersteams.Presentation/Scripts/enums/userSearchType.js
@@ -0,0 +1,11 @@
+﻿var app = angular.module("ttApp");
+
+app.value('UserSearchType', {
+    Friends: 1,
+    AllUsers: 2,
+
+    properties: {
+        1: { name: "Friends", value: 1 },
+        2: { name: "AllUsers", value: 2 }
+    }
+});
